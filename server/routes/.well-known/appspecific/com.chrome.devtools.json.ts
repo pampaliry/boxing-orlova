000e18f377df6f0ad69b34b433e66c92d249b85e
pampaliry <matko.matus22@gmail.com>
2025-09-29 12:00:41 +0200

    dist cleanup: keep only .output

--- server/routes/.well-known/appspecific/com.chrome.devtools.json.ts
+++
@@ -1,9 +0,0 @@
-export default defineEventHandler((event) => {
-  setHeader(event, 'Content-Type', 'application/json; charset=utf-8');
-  setResponseStatus(event, 204); // No Content (preferované)
-  return null;
-
-  // 200 s prázdnym objektom:
-  // setResponseStatus(event, 200)
-  // return {}
-});
