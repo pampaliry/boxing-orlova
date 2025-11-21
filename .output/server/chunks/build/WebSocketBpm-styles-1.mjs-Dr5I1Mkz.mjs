608c768dd0d801cb9f0ec77e42752c609f2ffc1e
pampaliry <matko.matus22@gmail.com>
2025-11-21 21:40:43 +0100

    Source snapshot 2025-11-21T21:40:43Z

--- .output/server/chunks/build/WebSocketBpm-styles-1.mjs-Dr5I1Mkz.mjs
+++
@@ -1,4 +0,0 @@
-const WebSocketBpm_vue_vue_type_style_index_0_scoped_eadeb469_lang = ".chart-wrap[data-v-eadeb469]{border:1px solid #e5e7eb;border-radius:8px;overflow:hidden}.chart[data-v-eadeb469]{display:block;height:auto;width:100%}";
-
-export { WebSocketBpm_vue_vue_type_style_index_0_scoped_eadeb469_lang as W };
-//# sourceMappingURL=WebSocketBpm-styles-1.mjs-Dr5I1Mkz.mjs.map
